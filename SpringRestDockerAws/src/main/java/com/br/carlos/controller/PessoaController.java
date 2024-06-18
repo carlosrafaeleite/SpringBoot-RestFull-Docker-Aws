ab2c7f47b5c72f02f5a9ac83a6a7ab550b6611a7
carlosrafaeleite <crafaelrosa@gmail.com>
2024-06-18 08:57:08 -0300

    finalizando crud

--- SpringRestDockerAws/src/main/java/com/br/carlos/controller/PessoaController.java
+++ SpringRestDockerAws/src/main/java/com/br/carlos/controller/PessoaController.java
@@ -5,7 +5,9 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,13 +28,35 @@ public class PessoaController implements Serializable {
 	public Pessoa pesquisaPorID(@PathVariable(value = "id") String id) throws Exception {
 		
 		return pessoaService.findById(id);
-		
 	}
+	
 	@RequestMapping(value = "/userAll", method = RequestMethod.GET, produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
 	public List<Pessoa> pesquisaAll(){
 		
 		return (List<Pessoa>) pessoaService.findall();
+	}
+	
+	@RequestMapping(value = "/cadPessoa", method = RequestMethod.POST,
+			produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE,
+			consumes = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+	public Pessoa cadastrarPessoa(@RequestBody Pessoa usuario ) throws Exception{
 		
+		return pessoaService.cadastrar(usuario);	
+	}
+	
+	@RequestMapping(value = "/atuPessoa", method = RequestMethod.PUT,
+			produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE,
+			consumes = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+	public Pessoa atualizarPessoa(@RequestBody Pessoa usuario ) throws Exception{
+		
+		return pessoaService.atualizar(usuario);	
+	}
+	
+
+	@RequestMapping(value = "excPessoa/{id}", method = RequestMethod.DELETE)
+	public void Excluir(@PathVariable(value = "id") String id) throws Exception {
+		
+		pessoaService.excluir(id);	
 	}
 
 }
--- SpringRestDockerAws/src/main/java/com/br/carlos/services/PessoaService.java
+++ SpringRestDockerAws/src/main/java/com/br/carlos/services/PessoaService.java
@@ -15,37 +15,39 @@ import com.br.carlos.model.Pessoa;
 public class PessoaService implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PessoaService.class.getName());
-	
-	public Pessoa findById(String id ) {
-		
+
+	public Pessoa findById(String id) {
+
 		Pessoa pessoa = new Pessoa();
 		pessoa.setId(counter.incrementAndGet());
 		pessoa.setNome("carlos");
 		pessoa.setSobrenome("rosa");
 		pessoa.setEndereco("ubatuba");
 		pessoa.setSexo("M");
-		
+
 		logger.info("Pessoa aqui");
-		return  pessoa;
-		
+		return pessoa;
+
 	}
+
 	public List<Pessoa> findall() {
-		
+
 		List<Pessoa> pessoaall = new ArrayList<Pessoa>();
-		
+
 		for (int i = 0; i < 8; i++) {
-			
+
 			Pessoa pessoa = mockPessoa(i);
 			pessoaall.add(pessoa);
 		}
-		
+
 		logger.info("Pessoa aqui All");
-		return  pessoaall;
-		
+		return pessoaall;
+
 	}
+
 	private Pessoa mockPessoa(int i) {
 		Pessoa pessoa = new Pessoa();
 		pessoa.setId(counter.incrementAndGet());
@@ -55,6 +57,23 @@ public class PessoaService implements Serializable {
 		pessoa.setSexo("M");
 
 		return pessoa;
+	}
+
+	public Pessoa cadastrar(Pessoa pessoa) {
+
+		logger.info("cadastrando Pessoa aqui");
+		return pessoa;
+	}
+	
+	public Pessoa atualizar(Pessoa pessoa) {
+		
+		logger.info("atualizando Pessoa aqui");
+		return pessoa;	
+	}
+	public void excluir(String id) {
+		
+		logger.info("excluindo Pessoa aqui");
+		
 	}
 
 }
