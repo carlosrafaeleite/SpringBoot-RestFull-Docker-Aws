9b3a00b2a4c5061c457a4aca2fe2f07dc8c942bf
carlosrafaeleite <crafaelrosa@gmail.com>
2024-06-29 21:52:45 -0300

    subindo exercicio

--- SpringRestDockerAws/src/main/java/com/br/carlos/controller/PessoaController.java
+++ SpringRestDockerAws/src/main/java/com/br/carlos/controller/PessoaController.java
@@ -4,7 +4,13 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,19 +28,19 @@ public class PessoaController implements Serializable {
 	@Autowired
 	PessoaService pessoaService;
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/{id}",produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
 	public Pessoa pesquisaPorID(@PathVariable(value = "id") Long id) throws Exception {
 		
 		return pessoaService.findById(id);
 	}
 	
-	@RequestMapping(value = "/userAll", method = RequestMethod.GET, produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/userAll",produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
 	public List<Pessoa> pesquisaAll(){
 		
 		return (List<Pessoa>) pessoaService.findall();
 	}
 	
-	@RequestMapping(value = "/cadPessoa", method = RequestMethod.POST,
+	@PostMapping(value = "/cadPessoa",
 			produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE,
 			consumes = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
 	public Pessoa cadastrarPessoa(@RequestBody Pessoa usuario ) throws Exception{
@@ -42,7 +48,7 @@ public class PessoaController implements Serializable {
 		return pessoaService.cadastrar(usuario);	
 	}
 	
-	@RequestMapping(value = "/atuPessoa", method = RequestMethod.PUT,
+	@PutMapping(value = "/atuPessoa",
 			produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE,
 			consumes = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
 	public Pessoa atualizarPessoa(@RequestBody Pessoa usuario ) throws Exception{
@@ -51,11 +57,13 @@ public class PessoaController implements Serializable {
 	}
 	
 
-	@RequestMapping(value = "excPessoa/{id}", method = RequestMethod.DELETE)
-	public void Excluir(@PathVariable(value = "id") Long id) throws Exception {
+	@DeleteMapping(value = "excPessoa/{id}")
+	public ResponseEntity<?> Excluir(@PathVariable(value = "id") Long id) throws Exception {
 		
 		pessoaService.excluir(id);	
+		return ResponseEntity.noContent().build();
 	}
+	
 
 }
 
--- SpringRestDockerAws/src/main/java/com/br/carlos/repository/PessoaRepository.java
+++ SpringRestDockerAws/src/main/java/com/br/carlos/repository/PessoaRepository.java
@@ -1,7 +1,5 @@
 package com.br.carlos.repository;
 
-import java.io.Serializable;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
